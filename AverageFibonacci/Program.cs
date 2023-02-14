3bc6f6cf1d190dbfe3beecceb5af8e79aaf8266f
kadircanarici <kadircanarc@gmail.com>
2023-02-14 16:08:24 +0300

    Add project files.

---
+++ AverageFibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Fibonacci serisinin derinliği: ");
+        int depth = int.Parse(Console.ReadLine());
+
+        var fib = new Fibonacci();
+        var mean = fib.Average(depth);
+
+        Console.WriteLine("Ortalama: " + mean);
+    }
+}
+
+class Fibonacci
+{
+    public double Average(int depth)
+    {
+        if (depth < 1) return 0;
+
+        double sum = 0;
+        int a = 0;
+        int b = 1;
+
+        for (int i = 1; i <= depth; i++)
+        {
+            int temp = b;
+            b = a + b;
+            a = temp;
+            sum += a;
+        }
+
+        return sum / depth;
+    }
+}
