0c35df94f61b7486bf6c3d3e1d17d489efe37ee6
kadircanarici <kadircanarc@gmail.com>
2023-02-14 18:13:27 +0300

    stringin bir indexteki değerini silme

---
+++ Algorithm-DeleteIndexOf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("String ve index değerini giriniz (örn: algoritma,3): ");
+        string input = Console.ReadLine();
+
+        // Kullanıcının girdiği değeri ',' karakterine göre ayırarak string ve integer değerlerini alıyoruz
+        string[] inputArr = input.Split(',');
+        string str = inputArr[0];
+        int index = int.Parse(inputArr[1]);
+
+        // İlgili indexteki karakteri çıkartarak ekrana yazdırıyoruz
+        if (index < str.Length)
+            Console.WriteLine(str.Remove(index, 1));
+        else
+            Console.WriteLine("Hatalı index değeri.");
+    }
+}
