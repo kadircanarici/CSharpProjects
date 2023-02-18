fb89292c575d7c343b83daf96327ec47b2e8b1de
kadircanarici <kadircanarc@gmail.com>
2023-02-18 23:17:48 +0300

    ilk ve son karakter değiştirme

---
+++ ChangingCharacter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input: ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length > 1)
+            {
+                char first = words[i][0];
+                char last = words[i][words[i].Length - 1];
+                words[i] = last + words[i].Substring(1, words[i].Length - 2) + first;
+            }
+        }
+
+        string output = String.Join(" ", words);
+        Console.WriteLine("Output: " + output);
+        Console.ReadKey();
+    }
+}
