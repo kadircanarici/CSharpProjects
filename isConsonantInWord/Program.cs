29a06bdd85e7b9e96435ef7019e96f2171b24220
kadircanarici <kadircanarc@gmail.com>
2023-02-18 23:29:20 +0300

    kelimede sessiz harf var mı

---
+++ isConsonantInWord/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SessizHarf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Metni girin: ");
+            string metin = Console.ReadLine();
+
+            string sessizler = "bcdfghjklmnpqrstvwxyz";
+            string[] kelimeler = metin.Split(' ');
+
+            foreach (string kelime in kelimeler)
+            {
+                bool sonuc = false;
+
+                for (int i = 0; i < kelime.Length - 1; i++)
+                {
+                    if (sessizler.Contains(kelime[i].ToString().ToLower()) && sessizler.Contains(kelime[i + 1].ToString().ToLower()))
+                    {
+                        sonuc = true;
+                        break;
+                    }
+                }
+
+                Console.Write(sonuc + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
